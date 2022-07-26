75bbf9c652821c519498d0691341c064008a3629
LozovskiiD <lozdm@yandex.ru>
2022-07-26 23:45:31 +0300

    test passed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+    const arNumbers = ['zero','one','two','three','four', 'five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen']
+  const arTen = ['ten','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety']
+  let strNum = number===0 ? 'zero': ''
+  let numInt = Math.trunc(number/100)
+  let numRest = number % 100    
+  strNum= numInt>0 ? arNumbers[numInt]+' hundred' : strNum
+  if (numRest === 0) {return strNum}
+
+  if (numRest>0 & numRest<20) {
+      strNum = strNum+' '+arNumbers[numRest]
+  }
+  else {
+      strNum = strNum+' '+arTen[Math.trunc(numRest/10-1)]  
+      strNum = numRest-Math.trunc(numRest/10)*10 >0 ? strNum+' '+ arNumbers[numRest-Math.trunc(numRest/10)*10] : strNum
+  }
+   return strNum.trim()
 }
